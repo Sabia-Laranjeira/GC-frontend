f53e34a794cd3c22a4d98a5b66d4b41d48e06a28
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-22 22:25:05 -0300

    [FEAT](cors): Removed cors in app.js file.

--- public/src/app.js
+++ public/src/app.js
@@ -1,11 +1,7 @@
 import express from 'express'
-import cors from 'cors'
 
 const app = express();
 
-app.use(cors({
-  origin: "https://gc-backend-wp6d.onrender.com"
-}))
 app.use(express.json());
 app.use(express.static('public'));
 
