d8b2505f699b4c344624805f30cddc85482c0edd
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-04-09 21:49:44 -0300

    [FEAT]: Now it's possible to select a item from the table.

--- src/components/JsonToTable/JsonToTable.jsx
+++ src/components/JsonToTable/JsonToTable.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 
 export default function JsonToTable({ json }) {
   if(!json) {
+    console.error(" 'json' prop in JsonToTable component is empty");
     return
   }
   const [selectedRow, selectRow] = useState(null);
 
+
+  const rowsRef = [];
+  for(let i = 0; i < json.length; i++) {
+    rowsRef[i] = useRef("");
+  }
+
   //get the object that contains more keys and make the <th> element.
   let headers = null;
+
   json.forEach((obj) => {
     let keys = Object.getOwnPropertyNames(obj);
     headers = keys;
@@ -16,9 +24,17 @@ export default function JsonToTable({ json }) {
       headers = keys;
     }
   })
-  const rows = json.map((obj) => <tr>{
-      headers.map((h,index) => <td key={index}>{obj[h]}</td>)
-      }</tr>);
+
+  const rows = json.map((obj,i) => <tr key={i} ref={rowsRef[i]} onClick={() => {
+        rowsRef.forEach((r) => {
+          r.current.className = "";
+        })
+        selectRow(json[i])
+        rowsRef[i].current.className = "selected-row";
+      }}>{
+        headers.map((h,index) => <td key={index}>{obj[h]}</td>)
+        }</tr>);
+
 
   return(
   <table>
@@ -33,16 +49,3 @@ export default function JsonToTable({ json }) {
   </table>
   )
 }
-
-/*
-<table>
-  <thead>
-    <tr>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><<tr>
-  </tbody>
-</table>
-
-*/
